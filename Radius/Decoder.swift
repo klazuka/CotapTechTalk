e1c506ff58d767d56b0cb0f7b12d602ac642b416
Keith Lazuka <kelazuka@microsoft.com>
2015-08-30 12:15:16 -0700

    Use a typealias to reduce line noise

--- Radius/Decoder.swift
+++ Radius/Decoder.swift
@@ -7,28 +7,30 @@ import Accelerate
 
 private let sampleRate = 48_000.0 as Float
 
+typealias FloatBuffer = UnsafeMutablePointer<Float>
+
 func decode() {
   let fftLength = 4096
   let setup = vDSP_DFT_zrop_CreateSetup(nil, vDSP_Length(fftLength), .FORWARD)
   
   // generate a test signal
-  let inputSamples = UnsafeMutablePointer<Float>.alloc(fftLength)
+  let inputSamples = FloatBuffer.alloc(fftLength)
   vDSP_vclr(inputSamples, 1, vDSP_Length(fftLength)) // clear to zero
   encode(0b0110011001100110, buffer: inputSamples, numSamples: fftLength)
   
   // de-interleave to get the audio input into the format that vDSP wants
-  let evenSamples = UnsafeMutablePointer<Float>.alloc(fftLength/2)
-  let oddSamples = UnsafeMutablePointer<Float>.alloc(fftLength/2)
+  let evenSamples = FloatBuffer.alloc(fftLength/2)
+  let oddSamples = FloatBuffer.alloc(fftLength/2)
   deinterleave(inputSamples, inputLength: fftLength, outputLeft: evenSamples, outputRight: oddSamples)
   
   // perform the DFT
-  let outReal = UnsafeMutablePointer<Float>.alloc(fftLength/2)
-  let outImaginary = UnsafeMutablePointer<Float>.alloc(fftLength/2)
+  let outReal = FloatBuffer.alloc(fftLength/2)
+  let outImaginary = FloatBuffer.alloc(fftLength/2)
   vDSP_DFT_Execute(setup, evenSamples, oddSamples, outReal, outImaginary)
   
   // compute magnitudes for each frequency bin
   var freqComplex = DSPSplitComplex(realp: outReal, imagp: outImaginary)
-  let freqMagnitudes = UnsafeMutablePointer<Float>.alloc(fftLength/2)
+  let freqMagnitudes = FloatBuffer.alloc(fftLength/2)
   vDSP_zvmags(&freqComplex, 1, freqMagnitudes, 1, vDSP_Length(fftLength/2))
   
   // print out the analysis
@@ -50,14 +52,14 @@ func decode() {
   freqMagnitudes.dealloc(fftLength/2)
 }
 
-private func deinterleave(input: UnsafePointer<Float>, inputLength: Int, outputLeft: UnsafeMutablePointer<Float>, outputRight: UnsafeMutablePointer<Float>) {
+private func deinterleave(input: UnsafePointer<Float>, inputLength: Int, outputLeft: FloatBuffer, outputRight: FloatBuffer) {
   
   // de-interleave the real samples by abusing vDSP's complex-number deinterleave function
   var out = DSPSplitComplex(realp: outputLeft, imagp: outputRight)
   vDSP_ctoz(UnsafePointer<DSPComplex>(input), 2, &out, 1, vDSP_Length(inputLength/2))
 }
 
-private func encode(token: UInt16, buffer: UnsafeMutablePointer<Float>, numSamples: Int) {
+private func encode(token: UInt16, buffer: FloatBuffer, numSamples: Int) {
 
   /// extract the bits in little-endian order
   func bits(n: UInt16) -> [Bit] {
@@ -78,7 +80,7 @@ private func encode(token: UInt16, buffer: UnsafeMutablePointer<Float>, numSampl
 }
 
 /// mixes a tone with frequency 'hz' into 'buffer'
-private func tone(hz: Float, buffer: UnsafeMutablePointer<Float>, numSamples: Int) {
+private func tone(hz: Float, buffer: FloatBuffer, numSamples: Int) {
   let x = hz * 2.0 * Float(M_PI) / sampleRate
   for i in 0..<numSamples {
     buffer[i] += sinf(Float(i) * x)
