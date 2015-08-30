e440788b85d4a0b0a1c9e3f2357aa5c65784ca94
Keith Lazuka <kelazuka@microsoft.com>
2015-08-30 11:10:37 -0700

    Compound tone gen

--- Radius/Decoder.swift
+++ Radius/Decoder.swift
@@ -8,21 +8,24 @@ import Accelerate
 private let sampleRate = 48_000.0 as Float
 
 func decode() {
-  let fftLength = 256
+  let fftLength = 512
   let setup = vDSP_DFT_zrop_CreateSetup(nil, vDSP_Length(fftLength), .FORWARD)
   
+  // generate a tone signal
   let inputSamples = UnsafeMutablePointer<Float>.alloc(fftLength)
-  tone(440, buffer: inputSamples, numSamples: fftLength)
+  tones([440, 880], buffer: inputSamples, numSamples: fftLength)
   
+  // de-interleave to get the audio input into the format that vDSP wants
   let evenSamples = UnsafeMutablePointer<Float>.alloc(fftLength/2)
   let oddSamples = UnsafeMutablePointer<Float>.alloc(fftLength/2)
   deinterleave(inputSamples, inputLength: fftLength, outputLeft: evenSamples, outputRight: oddSamples)
   
+  // perform the DFT
   let outReal = UnsafeMutablePointer<Float>.alloc(fftLength/2)
   let outImaginary = UnsafeMutablePointer<Float>.alloc(fftLength/2)
   vDSP_DFT_Execute(setup, evenSamples, oddSamples, outReal, outImaginary)
   
-  // compute magnitudes for each bin
+  // compute magnitudes for each frequency bin
   var freqComplex = DSPSplitComplex(realp: outReal, imagp: outImaginary)
   let freqMagnitudes = UnsafeMutablePointer<Float>.alloc(fftLength/2)
   vDSP_zvmags(&freqComplex, 1, freqMagnitudes, 1, vDSP_Length(fftLength/2))
@@ -53,12 +56,22 @@ private func deinterleave(input: UnsafePointer<Float>, inputLength: Int, outputL
   vDSP_ctoz(UnsafePointer<DSPComplex>(input), 2, &out, 1, vDSP_Length(inputLength/2))
 }
 
-// TODO consider using UnsafeMutableBufferPointer
+
+/// mixes a tone with frequency 'hz' into 'buffer'
+private func tones(frequencies: [Float], buffer: UnsafeMutablePointer<Float>, numSamples: Int) {
+  // clear to zero
+  vDSP_vclr(buffer, 1, vDSP_Length(numSamples))
+  // mix in each tone
+  for freq in frequencies {
+    tone(freq, buffer: buffer, numSamples: numSamples)
+  }
+}
+
+/// mixes a tone with frequency 'hz' into 'buffer'
 private func tone(hz: Float, buffer: UnsafeMutablePointer<Float>, numSamples: Int) {
   let x = hz * 2.0 * Float(M_PI) / sampleRate
-  
   for i in 0..<numSamples {
-    buffer[i] = sinf(Float(i) * x)
+    buffer[i] += sinf(Float(i) * x)
   }
 }
 
